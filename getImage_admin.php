45291fc4feebba91117d5d7f54303b698b3481d8
samjunan98 <95115804+samjunan98@users.noreply.github.com>
2022-06-30 01:07:29 +0800

    Update getImage_admin.php

--- getImage_admin.php
+++ getImage_admin.php
@@ -6,7 +6,7 @@ $adminID = $_SESSION['adminID'];
   $result = mysqli_query($db, $query);
   $row = mysqli_fetch_assoc($result);
   mysqli_close($db);
-
-  header("Content-type: https://petshopagentmanagementsystem.herokuapp.com/image/jpeg");
+  ob_clean();
+  header("Content-type: image/jpeg");
   echo $row['adminPhoto'];
 ?>
