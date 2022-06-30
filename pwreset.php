22058f70822e1d521306e40662b3cf8e02003469
samjunan98 <95115804+samjunan98@users.noreply.github.com>
2022-07-01 03:58:18 +0800

    Update pwreset.php

--- pwreset.php
+++ pwreset.php
@@ -20,7 +20,7 @@ if (isset($_POST['agentEmail']) & !empty($_POST['agentEmail'])) {
         $mail->Host = 'smtp.gmail.com';       // Specify main and backup SMTP servers 
         $mail->SMTPAuth = true;               // Enable SMTP authentication 
         $mail->Username = 'sampetshop2022@gmail.com';   // SMTP username 
-        $mail->Password = 'Sampetshop2022@';   // SMTP password 
+        $mail->Password = 'rafztuasivibfsdt';   // SMTP password 
         $mail->SMTPSecure = 'tls';            // Enable TLS encryption, `ssl` also accepted 
         $mail->Port = 587;                    // TCP port to connect to 
         // Sender info 
