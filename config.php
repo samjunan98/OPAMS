918e1a2f85142e8f2aaf3bb1f16ee0387787f6be
samjunan98 <95115804+samjunan98@users.noreply.github.com>
2022-07-02 16:39:40 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,12 @@
 <?php
+//    define('DB_SERVER', 'localhost');
+//    define('DB_USERNAME', 'root');
+//    define('DB_PASSWORD', '');
+//    define('DB_DATABASE', 'fypdb_test');
+//    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+//    if (!$db)
+// {die ('Error querying database. ');}
+
    define('DB_SERVER', 'us-cdbr-east-06.cleardb.net');
    define('DB_USERNAME', 'bc5674b7f014a2');
    define('DB_PASSWORD', '066b8068');
@@ -8,14 +16,3 @@
 {die ('Error querying database. ');}
 ?>
 
-<?php
-/*
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'fypdb_test');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-   if (!$db)
-{die ('Error querying database. ');}
-*/
-?>
